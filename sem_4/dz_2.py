2a432ad495c3da079727d2582f3b0c52047af586
Valet78 <valet78@mail.ru>
2022-12-03 15:43:38 +0300

    Решение 2 ДЗ 4-го семинара

--- sem_4/dz_2.py
+++ sem_4/dz_2.py
@@ -6,9 +6,34 @@
         144 -> [2,3]
 
 """
+from sympy import primefactors
+
+def SpisokRealOne(num: int):
+    listRes = []
+    temp, st = num, 0
+    for ind in [2, 3, 5, 7]:        
+        while temp % ind == 0: 
+            temp /= ind
+            st += 1        
+        if st > 0: 
+            listRes.append(ind)
+            temp, st = int(num / (ind**st)), 0 
+    if temp != 0: listRes.append(temp)
+    return listRes
 
 
-
+try:
+    inNum = int(input('Введите натуральное число N: '))
+    if inNum != 0:
+        print('\nВариант 1:')
+        print(f'\tДля N = {inNum} -> {SpisokRealOne(inNum)}')
+        print('\nВариант 2:')
+        print(f'\tДля N = {inNum} -> {primefactors(inNum)}\n')       
+        
+        
+    else: print('Введенно некорректное значение.')
+except:
+    print('Введенно некорректное значение.')
 
 
 
