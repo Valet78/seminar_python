ab0cce5324d25a83540d8b5f2b5d719db5b691d3
Valet78 <valet78@mail.ru>
2022-12-06 17:51:45 +0300

    Коррекция 4 (вариант) ДЗ 4-го семинара

---
+++ sem_4/dz_4v.py
@@ -0,0 +1,40 @@
+""" 
+Задание 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
+           (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+Пример:
+        k=2 => 2*x**2 + 4*x + 5 = 0 или x2 + 5 = 0 или 10*x**2 = 0
+        k=3 => 2*x**3 + 4*x**2 + 4*x + 5 = 0
+
+        Версия с использованием sympy
+"""
+
+from random import randint
+from sympy import simplify
+from os import getcwd
+pathFile = getcwd() + '\\sem_4\\file_4.txt'
+
+def SaveFile(strSave: str):
+    with open(pathFile, "a") as fileTxt:
+        fileTxt.write(strSave + '\n') 
+
+
+def GenPolinom(k: int) -> str:
+    temp = ''
+    for i in range(k + 1)[::-1]:
+        if i > 0: temp += f'{randint(0, 100)}*x**{i} + ' 
+        else: temp += f'{randint(0, 20)}*x**{i}'
+    return temp
+
+try:
+    inNumK = int(input('Введите значение натуральной степени k='))
+    if inNumK > 0:
+        strForFile = str(simplify(GenPolinom(inNumK)))        
+        SaveFile(f'{strForFile} = 0')    # Запишем полученное в файл
+    else: print('Введено некоректное значение.')
+    
+except ValueError:
+    print('Введено некоректное значение.')
+
+
+
